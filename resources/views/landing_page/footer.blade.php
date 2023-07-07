be2a1bddc31c83ffbea8b7a8533dd61c778670da
Riaa <maharaniria281@gmail.com>
2023-07-07 10:02:54 +0700

    Landing page correction

--- resources/views/landing_page/footer.blade.php
+++ resources/views/landing_page/footer.blade.php
@@ -2,36 +2,49 @@
 <footer class="footer-area section-gap">
     <div class="container">
         <div class="row">
-            <div class="col-lg-5 col-md-6 col-sm-6">
+            <div class="col-lg-7 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h6>About Us</h6>
                     <p>
                         DNA SPOT CAFE memiliki visi mengkombinasikan antara Cafe "luxurious“ (mewah) dan "affordable" (terjangkau semua kalangan), yang senantiasa berupaya memberikan layanan terbaik dan sajian produk berkualitas. Visi ini dimanifestasikan dengan misi DNA SPOT CAFE yaitu : Menyajikan varian menu cafe yang diminati konsumen dengan layanan terbaik untuk kepuasan konsumen.
                     </p>
                     <p class="footer-text">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
                     </p>								
                 </div>
             </div>
-            <div class="col-lg-5  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Contact Us</h6>
-                    <button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"> Whatsapp</i>
-                    </button>
-                </div>
-            </div>						
-            <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+            <div class="col-lg-3 col-md-6 col-sm-6 social-widget">
                 <div class="single-footer-widget">
                     <h6>Follow Us</h6>
                     <p>Let us be social</p>
                     <div class="footer-social d-flex align-items-center">
-                        <a href="#"><i class="fa fa-instagram"></i></a>
-                        <a href="#"><i class="fa fa-whatsApp"></i></a>
+                        <a href="https://www.instagram.com/dnaspotcafe/"><i class="fa fa-instagram"></i></a>
                     </div>
                 </div>
-            </div>							
+            </div>
+            <div class="col-lg-2  col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h6>Contact Us</h6>
+                    <p>Feel free to contact us</p>
+                    <div class="" id="mc_embed_signup">
+                        <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
+                            
+                            <a href="https://wa.me/6282124923532"><button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"> Whatsapp</i>
+                            </button>
+                            <div class="info pt-20"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>						
+            {{-- <div class="col-lg-3 col-md-6 col-sm-6 social-widget">
+                <div class="single-footer-widget">
+                    <h6>Follow Us</h6>
+                    <p>Let us be social</p>
+                    <div class="footer-social d-flex align-items-center">
+                        <a href="https://www.instagram.com/dnaspotcafe/"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>							 --}}
         </div>
     </div>
-</footer>	
+</footer>
